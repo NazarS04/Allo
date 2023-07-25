844347bf1e1497c12c73a82a9f749c6d86df35ae
Nazar <ns15052004@gmail.com>
2023-07-25 23:41:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import {Swiper} from "./swiper-bundle.min.js";
+
+new Swiper(".it-logitech__swiper",{
+  pagination:{
+    el:".it-logitech__pagination",
+    clickable: true,
+  }
+});

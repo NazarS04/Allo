8d6889ca8c2f5ea5fad24d90711b1aa3211eb0d3
Nazar <ns15052004@gmail.com>
2023-07-26 10:07:50 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,41 @@ new Swiper(".it-logitech__swiper",{
     clickable: true,
   }
 });
+
+new Swiper(".top-products__swiper",{
+  slidesPerView:1.2,
+  spaceBetween:16,
+  breakpoints:{
+    1200:{
+      slidesPerView:5,
+    },
+    992:{
+      slidesPerView:4.2,
+    },
+    768:{
+      slidesPerView:3.2,
+    },
+    450:{
+      slidesPerView:2.2,
+    }
+  }
+})
+
+new Swiper(".articles__swiper",{
+  slidesPerView:1.2,
+  spaceBetween:16,
+  breakpoints:{
+    1200:{
+      slidesPerView:5,
+    },
+    992:{
+      slidesPerView:4.2,
+    },
+    768:{
+      slidesPerView:3.2,
+    },
+    450:{
+      slidesPerView:2.2,
+    }
+  }
+})
